c1fe7d20841c248b358b2020a566b67ac957a800
shoet <32211937+shoet@users.noreply.github.com>
2023-12-14 08:41:55 +0900

    更新処理を修正

--- entities/summary.go
+++ entities/summary.go
@@ -2,10 +2,10 @@ package entities
 
 type Summary struct {
 	Id               string `json:"id" dynamodbav:"id"`
-	TaskStatus       string `json:"taskStatus" dynamodbav:"task_status"`
-	PageUrl          string `json:"pageUrl" dynamodbav:"page_url"`
-	Title            string `json:"title,omitempty" dynamodbav:"title"`
-	Content          string `json:"content,omitempty" dynamodbav:"content"`
-	Summary          string `json:"summary" dynamodbav:"summary"`
-	TaskFailedReason string `json:"taskFailedReason,omitempty" dynamodbav:"task_failed_reason"`
+	TaskStatus       string `json:"taskStatus" dynamodbav:"task_status,omitempty"`
+	PageUrl          string `json:"pageUrl" dynamodbav:"page_url,omitempty"`
+	Title            string `json:"title,omitempty" dynamodbav:"title,omitempty"`
+	Content          string `json:"content,omitempty" dynamodbav:"content,omitempty"`
+	Summary          string `json:"summary" dynamodbav:"summary,omitempty"`
+	TaskFailedReason string `json:"taskFailedReason,omitempty" dynamodbav:"task_failed_reason,omitempty"`
 }
