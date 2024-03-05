5e59cd49ea58d68f528ddd00d9a702279b7130dc
shoet <32211937+shoet@users.noreply.github.com>
2024-03-05 09:47:22 +0900

    read用のテーブルを作成

---
+++ misc/db/migrations/postgres/20240305092931-init.sql
@@ -0,0 +1,14 @@
+
+-- +migrate Up
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY,
+  task_id VARCHAR(255) NOT NULL,
+  task_status VARCHAR(255) NOT NULL,
+  title TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- +migrate Down
+drop table tasks;
+
