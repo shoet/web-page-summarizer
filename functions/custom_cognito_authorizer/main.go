dc5e7df23d0ff9a9bd995679ad9061c562b618a4
shoet <32211937+shoet@users.noreply.github.com>
2024-03-30 14:52:16 +0900

    Authorizerがdummyでもx-api-keyがセットされていればリクエストを通す

--- functions/custom_cognito_authorizer/main.go
+++ functions/custom_cognito_authorizer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 
@@ -13,36 +14,68 @@ import (
 
 type Config struct {
 	CognitoJWKUrl string `env:"COGNITO_JWK_URL,required"`
+	APIKey        string `env:"API_KEY,required"`
 }
 
-func Handle(ctx context.Context, req events.APIGatewayCustomAuthorizerRequestTypeRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
-	var config Config
-	if err := env.Parse(&config); err != nil {
-		return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("failed to parse config: %w", err)
-	}
-	authorization, ok := req.Headers["Authorization"]
-	if !ok {
-		return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("Authorization header not found")
-	}
-	accessToken := strings.Replace(authorization, "Bearer ", "", 1)
-	if _, err := util.VerifyToken(ctx, config.CognitoJWKUrl, accessToken); err != nil {
-		return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("failed to verify token: %w", err)
-	}
-	return events.APIGatewayCustomAuthorizerResponse{
-		PrincipalID: "user",
-		PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
-			Version: "2012-10-17",
-			Statement: []events.IAMPolicyStatement{
-				{
-					Action:   []string{"execute-api:Invoke"},
-					Effect:   "Allow",
-					Resource: []string{"arn:aws:execute-api:*:*:*"},
-				},
+var allowResponse = events.APIGatewayCustomAuthorizerResponse{
+	PrincipalID: "user",
+	PolicyDocument: events.APIGatewayCustomAuthorizerPolicy{
+		Version: "2012-10-17",
+		Statement: []events.IAMPolicyStatement{
+			{
+				Action:   []string{"execute-api:Invoke"},
+				Effect:   "Allow",
+				Resource: []string{"arn:aws:execute-api:*:*:*"},
 			},
 		},
+	},
+}
+
+type CustomCognitoAuthorizerHandler struct {
+	CognitoJWKUrl string
+	ApiKey        string
+}
+
+func NewCustomCognitoAuthorizerHandler() (*CustomCognitoAuthorizerHandler, error) {
+	var config Config
+	if err := env.Parse(&config); err != nil {
+		return nil, fmt.Errorf("failed to parse config: %w", err)
+	}
+	return &CustomCognitoAuthorizerHandler{
+		CognitoJWKUrl: config.CognitoJWKUrl,
+		ApiKey:        config.APIKey,
 	}, nil
 }
 
+func (c *CustomCognitoAuthorizerHandler) Handle(
+	ctx context.Context, req events.APIGatewayCustomAuthorizerRequestTypeRequest,
+) (events.APIGatewayCustomAuthorizerResponse, error) {
+	apiKey, ok := req.Headers["x-api-key"]
+	if ok && apiKey == c.ApiKey {
+		fmt.Printf("api key is valid\n")
+		return allowResponse, nil
+	}
+
+	authorization, ok := req.Headers["Authorization"]
+	if !ok {
+		fmt.Printf("authorization header not found\n")
+		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized")
+	}
+	accessToken := strings.Replace(authorization, "Bearer ", "", 1)
+	if _, err := util.VerifyToken(ctx, c.CognitoJWKUrl, accessToken); err != nil {
+		fmt.Printf("failed to verify token: %s\n", err)
+		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized")
+	}
+	fmt.Printf("token is valid\n")
+	fmt.Printf("token: %s\n", accessToken)
+	return allowResponse, nil
+}
+
 func main() {
-	lambda.Start(Handle)
+	h, err := NewCustomCognitoAuthorizerHandler()
+	if err != nil {
+		fmt.Printf("failed to create handler: %s\n", err)
+		panic(err)
+	}
+	lambda.Start(h.Handle)
 }
